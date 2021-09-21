179f9d5fe7d5519236e1204f5d9de6ce6fe6ba35
Paul <pauladriancaga@gmail.com>
2021-09-21 11:50:22 +0800

    20210921PAC: fixed app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ResultTableComponent } from './Shared Components/result-table/result-table.component';
-import { HeaderComponent } from './Shared Components/header/header.component';
 import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
-    ResultTableComponent,
-    HeaderComponent
+    ResultTableComponent
   ],
   imports: [
     BrowserModule,

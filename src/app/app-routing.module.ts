d83eea658da13b4e981c3e4a75fe83920c519b70
Paul <pauladriancaga@gmail.com>
2021-09-21 13:22:21 +0800

    20210921PAC: blog header route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './Shared Components/header/header.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'blog', component: HeaderComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

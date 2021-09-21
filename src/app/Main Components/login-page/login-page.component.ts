31cbf4c4b43e567caadb56d0fcf2e252272bfc2b
andrejustinfernando <andrejustinfernando@yahoo.com>
2021-09-21 16:48:19 +0800

    20210921AJF: Login Page Frontend and Functions

--- src/app/Main Components/login-page/login-page.component.ts
+++ src/app/Main Components/login-page/login-page.component.ts
@@ -1,15 +1,59 @@
 import { Component, OnInit } from '@angular/core';
-
+import { UserService } from 'src/app/Services/user.service';
+import { User } from 'src/app/models/User';
+import {
+  FormControl,
+  FormGroup,
+  FormBuilder,
+  Validators,
+} from '@angular/forms';
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
-  styleUrls: ['./login-page.component.css']
+  styleUrls: ['./login-page.component.css'],
 })
 export class LoginPageComponent implements OnInit {
+  users: User[] = [];
+  validatePass: any;
+  userType!: any;
 
-  constructor() { }
+  loginForm: any;
 
-  ngOnInit(): void {
+  constructor(
+    private formBuilder: FormBuilder,
+    private userService: UserService
+  ) {
+    this.loginForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
+  get username() {
+    return this.loginForm.get('username');
   }
 
+  get password() {
+    return this.loginForm.get('password');
+  }
+
+  ngOnInit(): void {
+    this.userService.getUsers().subscribe((users) => (this.users = users));
+  }
+
+  //Console log first since author and approver pages are not done yet
+  navigateTo(userType: string) {
+    console.log(userType);
+  }
+
+  validateUser() {
+    this.validatePass = this.users.find(
+      (x) => x.username === this.loginForm.value.username
+    )?.password;
+    if (this.validatePass == this.loginForm.value.password) {
+      this.userType = this.users.find(
+        (x) => x.username === this.loginForm.value.username
+      )?.userType;
+      this.navigateTo(this.userType);
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,21 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { LoginPageComponent } from './Main Components/login-page/login-page.component';
 
 @NgModule({
   declarations: [AppComponent, LoginPageComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    RouterModule.forRoot([{ path: 'login', component: LoginPageComponent }]),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })

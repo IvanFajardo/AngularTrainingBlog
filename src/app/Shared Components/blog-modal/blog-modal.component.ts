81e0a51894e41b82b14feb1c440ba6fbb42f0771
andrejustinfernando <andrejustinfernando@yahoo.com>
2021-09-23 13:05:54 +0800

    20210923AJF: Blog Modal Frontend & Functionalities

--- src/app/Shared Components/blog-modal/blog-modal.component.ts
+++ src/app/Shared Components/blog-modal/blog-modal.component.ts
@@ -1,20 +1,49 @@
-import { Component, OnInit, TemplateRef } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  TemplateRef,
+  Input,
+  Output,
+  EventEmitter,
+} from '@angular/core';
 import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
+import { FormBuilder, FormControl } from '@angular/forms';
 @Component({
   selector: 'app-blog-modal',
   templateUrl: './blog-modal.component.html',
   styleUrls: ['./blog-modal.component.css'],
 })
 export class BlogModalComponent implements OnInit {
-  public modalRef!: BsModalRef;
-  constructor(private modalService: BsModalService) {}
+  @Output() buttonEmitter = new EventEmitter();
+  @Input() content?: FormControl;
+  @Input() remarks?: FormControl;
+  @Input() title?: string;
+  @Input() type?: string;
+
+  blogForm: any;
+  public modalRef?: BsModalRef;
+  constructor(
+    private modalService: BsModalService,
+    private formBuilder: FormBuilder
+  ) {
+    this.blogForm = this.formBuilder.group({
+      content: [''],
+      remarks: [''],
+    });
+  }
+
   ngOnInit(): void {}
 
-  public openModal(template: TemplateRef<any>) {
+  openModal(template: TemplateRef<any>) {
     this.modalRef = this.modalService.show(template);
   }
 
-  emitButton() {}
+  emitButton(buttonName: string) {
+    console.log(buttonName);
+    this.buttonEmitter.emit(buttonName);
+  }
 
-  close() {}
+  close() {
+    this.modalRef?.hide();
+  }
 }

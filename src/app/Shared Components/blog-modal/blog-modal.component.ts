96485b163da0ad7b1a1cd3918653d76b870e71cc
andrejustinfernando <andrejustinfernando@yahoo.com>
2021-09-24 10:56:26 +0800

    20210924AJF: Added Title Input

--- src/app/Shared Components/blog-modal/blog-modal.component.ts
+++ src/app/Shared Components/blog-modal/blog-modal.component.ts
@@ -17,7 +17,7 @@ export class BlogModalComponent implements OnInit {
   @Output() buttonEmitter = new EventEmitter();
   @Input() content = new FormControl();
   @Input() remarks = new FormControl();
-  @Input() title?: string = 'Sample Blog';
+  @Input() title = new FormControl();
   @Input() type?: string;
 
   public modalRef?: BsModalRef;
@@ -32,6 +32,7 @@ export class BlogModalComponent implements OnInit {
   emitButton(buttonName: string) {
     console.log(this.content.value);
     console.log(this.remarks.value);
+    console.log(this.title.value);
     console.log(buttonName);
     this.buttonEmitter.emit(buttonName);
   }
